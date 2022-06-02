65977a6b3c014191b10c852ef106cde195cd3529
Narendrakumar Hosamani <43786432+Narendra1508@users.noreply.github.com>
2022-06-02 09:12:56 +0200

    Update Pyspark.py

--- Pyspark.py
+++ Pyspark.py
@@ -1,3 +1,4 @@
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 data2 = [("James","","Smith","36636","M",3000),
     ("Michael","Rose","","40288","M",4000),
     ("Robert","","Williams","42114","M",4000),
